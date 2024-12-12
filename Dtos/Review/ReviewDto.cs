0865338f8c67ca7e5cfe68d8c1cf444ee5d5e7b3
codinghaki <113092980+codinghaki@users.noreply.github.com>
2024-12-12 21:43:43 +1100

    Implement data validation on Review dtos

--- Dtos/Review/ReviewDto.cs
+++ Dtos/Review/ReviewDto.cs
@@ -1,3 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Rate_My_Units_API.Dtos;
 
-public record ReviewDto(string Content, int Score, DateTime CreatedAt);
+public record ReviewDto(
+    [Required]
+    [MinLength(5, ErrorMessage = "Content must be at least 5 characters")]
+    [MaxLength(1000, ErrorMessage = "Content cannot exceed 1000 characters")]
+    string Content,
+    [Required]
+    [Range(1, 10, ErrorMessage = "Score must be between 1 and 10")]
+    int Score,
+    DateTime CreatedAt);
